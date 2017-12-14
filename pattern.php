46cf85b9044670147c3166d7bf7aa1ae7a8de717
yogesh mishra <yogesh.mishra@wholesalebox.co>
2017-12-14 14:54:04 +0530

    first commit

---
+++ pattern.php
@@ -0,0 +1,35 @@
+<?php
+
+echo "please enter the value of i"." \n";
+$n = fgets(STDIN);
+
+
+for($i=0; $i<$n; $i++){
+	
+	for($j=0; $j<=$i ; $j++){
+		echo "*";
+	}
+
+	echo "\n";
+
+	// for($j=0; $j<=$i ; $j++){
+	// 	echo "*";
+	// }
+
+	// echo "\n";
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ send_sms.php
@@ -0,0 +1,35 @@
+<?php
+
+// SMS Sending Block of code
+           $request = ""; //initialise the request variable
+           $param['method'] = "sendMessage";
+           $param['send_to'] = "91" . "8824179288";
+           $param['msg'] = "Test SMS from helpdesk development";
+           $param['userid'] = "2000150141";
+           $param['password'] = "SDLPu7cs2N";
+           $param['v'] = "1.1";
+           $param['msg_type'] = "TEXT"; //Can be "FLASH”/"UNICODE_TEXT"/”BINARY”
+           $param['auth_scheme'] = "PLAIN";
+
+//Have to URL encode the values
+           foreach ($param as $key => $val) {
+               $request .= $key . "=" . urlencode($val);
+//we have to urlencode the values
+               $request .= "&";
+//append the ampersand (&) sign after each parameter/value pair
+           }
+           $request = substr($request, 0, strlen($request) - 1);
+//remove final (&) sign from the request
+           $url = "http://enterprise.smsgupshup.com/GatewayAPI/rest?" . $request;
+echo $url; die;
+           $ch = curl_init($url);
+           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+           $curl_scraped_page = curl_exec($ch);
+// print_r($curl_scraped_page ); 
+           curl_close($ch);
+           $response = explode('|', $curl_scraped_page);
+
+           print_r($response); 
+ 
+
+?>   
